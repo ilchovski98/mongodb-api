b55507d5f07e66061cea0c17b3d1fd3a9b223f70
ilchovski98 <ilchovski98@gmail.com>
2022-09-13 06:19:24 +0300

    Add validation logic to mongoose Tweet

--- models/tweets.js
+++ models/tweets.js
@@ -4,6 +4,20 @@ const Tweet = new mongoose.Schema({
   text: {
     type: String,
     required: true
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ['postman', 'work', 'personal']
+  },
+  tags: {
+    type: Array,
+    validate: {
+      validator: function(value) {
+        return value && value.length > 0;
+      },
+      message: 'A course should have at least one tag.'
+    }
   }
 });
 
